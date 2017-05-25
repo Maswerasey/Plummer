224c4afd049beec21e4d2198961c84a907216ad2
Jeremy Heyl <heyl@phas.ubc.ca>
2017-05-24 19:32:13 -0700

    added plot

---
+++ hernSigma.sh
@@ -0,0 +1,1 @@
+ctioga2 --name hernSigma --ylog hernquist.dat@1:3 @1:6  @1:7 @1:9 -x '$R/a$' -y '$\Sigma, \sigma_G^2, \sigma_{BH}^2$' --xlog --draw-line -2,1.25 1,-1.75 --draw-line -2,1.6989 1,-1.3010
